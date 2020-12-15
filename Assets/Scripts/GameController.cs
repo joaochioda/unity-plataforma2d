3e5a36b882c06b706317724eb21f63ad498d3489
joaochioda <joao.chioda@dextra-sw.com>
2020-12-15 20:32:12 -0300

    aula 9

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int totalScore;
+    public Text scoreText;
+    public static GameController instance;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScoreText()
+    {
+        scoreText.text = totalScore.ToString();
+    }
+}
