4164f460db0949a12a01ab557b562207b6c8b4d5
joaochioda <joao.chioda@dextra-sw.com>
2020-12-20 00:32:59 -0300

    inimigo

---
+++ Assets/Scripts/Frog.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private Animator anim;
+
+    public float speed;
+
+    public Transform rightCol;
+    public Transform leftCol;
+    public Transform headPoint;
+
+    public LayerMask layer;
+
+    private bool colliding;
+
+    public BoxCollider2D boxCollider2D;
+    public CircleCollider2D circleCollider2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new Vector2(speed, rig.velocity.y);
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position);
+        if (colliding)
+        {
+            transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
+            speed *= -1f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            float height = collision.contacts[0].point.y - headPoint.position.y;
+            if (height >0)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+                anim.SetTrigger("die");
+                boxCollider2D.enabled = false;
+                circleCollider2D.enabled = false;
+                rig.bodyType = RigidbodyType2D.Dynamic;
+                Destroy(gameObject, 0.4f);
+                speed = 0;
+            }
+        }
+    }
+}
